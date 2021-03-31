13b77ca305ad551ee362d394922f3ea8c02dec91
unknown <jobocanegra@personalsoft.com.co>
2021-03-31 11:02:31 -0500

    fixed importated package name FlutterTestRunner

--- android/app/src/androidTest/java/com/example/driver_app/MainActivityTest.java
+++ android/app/src/androidTest/java/com/example/driver_app/MainActivityTest.java
@@ -1,13 +1,13 @@
 package com.example.driver_app;
 
 import androidx.test.rule.ActivityTestRule;
-import dev.flutter.plugins.integration_test.FlutterRunnerTest;
+import dev.flutter.plugins.integration_test.FlutterTestRunner;
 import org.unit.Rule;
 import org.unit.runner.RunWith;
 import com.example.driver_app.MainActivity;
 
-@RunWith(FlutterRunnerTest.class)
+@RunWith(FlutterTestRunner.class)
 public class MainActivityTest {
   @Rule
-  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class, true, false);
 }
